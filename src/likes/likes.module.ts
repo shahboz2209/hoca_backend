a4256e8cd5caa779cd2c2c1391c4bafd08b7d791
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-09-15 09:25:39 +0300

    imports

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -3,9 +3,11 @@ import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Likes } from './models/likes.model';
+import { User } from '../user/models/user.model';
+import { Messages } from '../messages/models/messages.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Likes])],
+  imports: [SequelizeModule.forFeature([Likes, User, Messages])],
   controllers: [LikesController],
   providers: [LikesService],
 })
