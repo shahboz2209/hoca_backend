210374c5ac98ceb8b79bf97dfcb7398ac511bad9
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-09-15 09:43:35 +0300

    message

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,11 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Comments } from './models/comments.model';
+import { User } from '../user/models/user.model';
+import { Messages } from '../messages/models/messages.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Comments])],
+  imports: [SequelizeModule.forFeature([Comments, User, Messages])],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- src/dislikes/dislikes.module.ts
+++ src/dislikes/dislikes.module.ts
@@ -3,9 +3,11 @@ import { DislikesService } from './dislikes.service';
 import { DislikesController } from './dislikes.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Dislikes } from './models/dislikes.model';
+import { User } from '../user/models/user.model';
+import { Messages } from '../messages/models/messages.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Dislikes])],
+  imports: [SequelizeModule.forFeature([Dislikes, User, Messages])],
   controllers: [DislikesController],
   providers: [DislikesService],
 })
